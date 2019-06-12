d97006dd50cb43fd6f762ab4964bd89c653ed7a0
Cody Garrett <cody.garrett@fineos.com>
2019-06-12 15:50:44 -0400

    Updated the Args to support all the hostname/port/protocol/etc. when launching

--- src/com/garrett_tech/simple_selenium/application/Program.java
+++ src/com/garrett_tech/simple_selenium/application/Program.java
@@ -7,26 +7,13 @@ import com.garrett_tech.simple_selenium.tests.ExampleTest_NavigateToGithub;
 public class Program {
 		
     public static void main(String[] args) throws Exception{
-    	
-    	
-    	
-    	setArguments(args);
-    	ExampleTest_NavigateToGithub.navigate();
-    }
-    
-	private static void setArguments(String[] args)
-	{
-    	System.out.println("Running Simple Selenium with the following Arguments:");
-
-		for(String arg : args)
-		{
-			System.out.println(arg);
-			
-			if(arg.contains(Constants.arg_driver_path))
-			{
-				arg = arg.replace(Constants.arg_driver_path, "");
-				System.setProperty("webdriver.chrome.driver", arg);
-			}
+    	try {
+			Constants.setArguments(args);
+			ExampleTest_NavigateToGithub.navigate();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-	}
+    	
+    	System.exit(0);
+    }
 }
--- src/com/garrett_tech/simple_selenium/etc/Constants.java
+++ src/com/garrett_tech/simple_selenium/etc/Constants.java
@@ -19,6 +19,12 @@ public class Constants {
 	public static final String actionType_select_dropdown = "select_dropdown";
 
 	public static final String arg_driver_path = "--driver-path=";
+	public static final String arg_driver_username = "--driver-username=";
+	public static final String arg_driver_password = "--driver-password=";
+	public static final String arg_driver_hostname = "--driver-hostname=";
+	public static final String arg_driver_basePath = "--driver-basePath=";
+	public static final String arg_driver_protocol = "--driver-protocol=";
+	public static final String arg_driver_port = "--driver-port=";
 
 	public static List<String> getArgs() {
 		return new ArrayList<String>() {
@@ -26,5 +32,57 @@ public class Constants {
 				this.add(arg_driver_path);
 			}
 		};
+	}
+	
+	public static void setArguments(String[] args)
+	{
+    	System.out.println("Running with the following Arguments:");
+
+		for(String arg : args)
+		{
+			System.out.println(arg);
+			
+			if(arg.contains(Constants.arg_driver_path))
+			{
+				arg = arg.replace(Constants.arg_driver_path, "");
+				System.setProperty("webdriver.chrome.driver", arg);
+			}
+
+			if(arg.contains(Constants.arg_driver_username))
+			{
+				arg = arg.replace(Constants.arg_driver_username, "");
+				Constants.driver_username = arg;
+			}
+			
+			if(arg.contains(Constants.arg_driver_password))
+			{
+				arg = arg.replace(Constants.arg_driver_password, "");
+				Constants.driver_password = arg;
+			}
+			
+			if(arg.contains(Constants.arg_driver_hostname))
+			{
+				arg = arg.replace(Constants.arg_driver_hostname, "");
+				Constants.driver_hostname = arg;
+			}
+			
+			if(arg.contains(Constants.arg_driver_basePath))
+			{
+				arg = arg.replace(Constants.arg_driver_basePath, "");
+				Constants.driver_basePath = arg;
+			}
+			
+			if(arg.contains(Constants.arg_driver_protocol))
+			{
+				arg = arg.replace(Constants.arg_driver_protocol, "");
+				Constants.driver_protocol = arg;
+			}
+			
+			if(arg.contains(Constants.arg_driver_port))
+			{
+				arg = arg.replace(Constants.arg_driver_port, "");
+				Constants.driver_port = arg;
+			}
+		}
 	}
 }
